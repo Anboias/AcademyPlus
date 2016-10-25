32777343af3fe509a9a253066b16194daeef08f9
Anboias <biasinovschi@yahoo.ro>
2016-10-26 02:27:05 +0300

    -

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,18 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: biasinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/10/25 20:24:10 by biasinov          #+#    #+#             */
-/*   Updated: 2016/10/26 01:36:33 by biasinov         ###   ########.fr       */
+/*   Created: 2016/10/26 01:18:32 by biasinov          #+#    #+#             */
+/*   Updated: 2016/10/26 02:25:15 by biasinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar(char c)
+void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putchar_fd(c, 1);
+	int rev;
+
+	rev = 0;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+	}
+	if (n == 0)
+	{
+		ft_putchar_fd('0', fd);
+		return ;
+	}
+	while (n > 0)
+	{
+		rev = (rev * 10) + n % 10;
+		n /= 10;
+	}
+	while (rev > 0)
+	{
+		ft_putchar_fd((rev % 10) + '0', fd);
+		rev /= 10;
+	}
 }
