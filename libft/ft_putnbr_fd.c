59c6f2dc8f00778c2615c1a77364be945f8c088b
Anboias <biasinovschi@yahoo.ro>
2016-12-05 09:41:45 +0200

    remade

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: biasinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/26 01:18:32 by biasinov          #+#    #+#             */
-/*   Updated: 2016/10/31 15:14:51 by biasinov         ###   ########.fr       */
+/*   Updated: 2016/12/05 09:38:24 by biasinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,18 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int rev;
-
-	rev = 0;
 	if (n < 0)
 	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
+		ft_putchar('-');
+		n = -n;
 	}
-	if (n == 0)
+	if (n >= 10)
 	{
-		ft_putchar_fd('0', fd);
-		return ;
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
-	while (n > 0)
+	else
 	{
-		rev = (rev * 10) + n % 10;
-		n /= 10;
-	}
-	while (rev > 0)
-	{
-		ft_putchar_fd((rev % 10) + '0', fd);
-		rev /= 10;
+		ft_putchar(n + '0');
 	}
 }
